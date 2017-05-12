01d198a4dd5d96e28c03e044268830285968e880
MYFBILL <nflsmyf@sina.cn>
2017-05-13 04:06:07 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function set and get the value and mean of the matrix
+## It outputs a list of the four functions defined
 
 makeCacheMatrix <- function(x = matrix()) {
   inver<-NULL
@@ -19,6 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## When it calls the function cacheSolve, it will check whether the
+## Mean has been calculated and stored
+## If yes, it will directly output the value, otherwise it will 
+## Calculate first and output
 
 cacheSolve <- function(x, ...) {
   inver<-x$getinverse()
